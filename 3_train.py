3c086928bc60226b23404d7ab5cf59880fa6ee5d
Learner4everrr <zhan8023@umn.edu>
2024-04-09 10:06:25 -0500

    upload

--- 3_train.py
+++ 3_train.py
@@ -57,64 +57,64 @@ print("2"*50)
 
 
 
-# # # # model_id = '/scratch/ahcie-gpu2/openllama-models/MedLLaMA_13B'
-# # # model_id="/scratch/ahcie-gpu2/llama-models-meta-hf/Llama-2-13b-hf"
-# model_id = "meta-llama/Llama-2-13b-hf"
-# # model_id = "medalpaca/medalpaca-13b"
+# # # model_id = '/scratch/ahcie-gpu2/openllama-models/MedLLaMA_13B'
+# # model_id="/scratch/ahcie-gpu2/llama-models-meta-hf/Llama-2-13b-hf"
+model_id = "meta-llama/Llama-2-13b-hf"
+# model_id = "medalpaca/medalpaca-13b"
 
-# qlora_config = LoraConfig(
-#     r=64,
-#     lora_alpha=32,
-#     lora_dropout=0.1,
-#     bias="none",
-#     task_type="CAUSAL_LM"
-# )
+qlora_config = LoraConfig(
+    r=64,
+    lora_alpha=32,
+    lora_dropout=0.1,
+    bias="none",
+    task_type="CAUSAL_LM"
+)
 
-# bnb_config = BitsAndBytesConfig(
-#     load_in_4bit=True,
-#     bnb_4bit_use_double_quant=True,
-#     bnb_4bit_quant_type="nf4",
-#     bnb_4bit_compute_dtype=torch.bfloat16
-# )
+bnb_config = BitsAndBytesConfig(
+    load_in_4bit=True,
+    bnb_4bit_use_double_quant=True,
+    bnb_4bit_quant_type="nf4",
+    bnb_4bit_compute_dtype=torch.bfloat16
+)
 
-# base_model = AutoModelForCausalLM.from_pretrained(
-#     model_id,
-#     quantization_config=bnb_config,torch_dtype=torch.float16, device_map='auto',
-# )
+base_model = AutoModelForCausalLM.from_pretrained(
+    model_id,
+    quantization_config=bnb_config,torch_dtype=torch.float16, device_map='auto',
+)
 
-# print("3"*50)
+print("3"*50)
 
 
-# tokenizer = AutoTokenizer.from_pretrained(model_id)
-# tokenizer.add_special_tokens({'pad_token': '[PAD]'})
-# # tokenizer.add_special_tokens({'end_token': '[END]'})
+tokenizer = AutoTokenizer.from_pretrained(model_id)
+tokenizer.add_special_tokens({'pad_token': '[PAD]'})
+# tokenizer.add_special_tokens({'end_token': '[END]'})
 
-# print("4"*50)
+print("4"*50)
 
-# supervised_finetuning_trainer = SFTTrainer(
-#     base_model,
-#     train_dataset=train,
-#     eval_dataset=test,
-#     args=transformers.TrainingArguments(
-#         per_device_train_batch_size=1,
-#         gradient_accumulation_steps=4,
-#         learning_rate=2e-4,
-#         max_steps=5000,
-#         max_grad_norm=0.3,
-#         warmup_ratio=0.03,
-#         output_dir="Our_model",
-#         optim="paged_adamw_8bit",
-#         fp16=True,
-#         evaluation_strategy = "steps",
-#         eval_steps = 50,
-#         save_steps = 50,
-#         load_best_model_at_end=True,
-#         save_strategy='steps',
-#     ),
-#     tokenizer=tokenizer,
-#     peft_config=qlora_config,
-#     dataset_text_field="text",
-#     max_seq_length=4098
-# )
+supervised_finetuning_trainer = SFTTrainer(
+    base_model,
+    train_dataset=train,
+    eval_dataset=test,
+    args=transformers.TrainingArguments(
+        per_device_train_batch_size=1,
+        gradient_accumulation_steps=4,
+        learning_rate=2e-4,
+        max_steps=5000,
+        max_grad_norm=0.3,
+        warmup_ratio=0.03,
+        output_dir="Our_model",
+        optim="paged_adamw_8bit",
+        fp16=True,
+        evaluation_strategy = "steps",
+        eval_steps = 50,
+        save_steps = 50,
+        load_best_model_at_end=True,
+        save_strategy='steps',
+    ),
+    tokenizer=tokenizer,
+    peft_config=qlora_config,
+    dataset_text_field="text",
+    max_seq_length=2048  #4098
+)
 
-# supervised_finetuning_trainer.train()
+supervised_finetuning_trainer.train()
