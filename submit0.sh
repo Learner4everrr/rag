dfda0c0360455de19badeec6fdaa925a2ceb5bdc
Learner4everrr <zhan8023@umn.edu>
2024-04-22 21:36:06 -0500

    update

--- submit0.sh
+++ submit0.sh
@@ -3,7 +3,7 @@
 #SBATCH -p dos-chs,a100-4,agsmall,ag2tb,a100-8
 #SBATCH --gres=gpu:a100:1
 #SBATCH --mem=150g
-#SBATCH --time=36:00:00
+#SBATCH --time=24:00:00
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=zhan8023@umn.edu
 
